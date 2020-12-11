743f565c872091d4531227ebaadd9516c2dcd747
Edgar Alejandro Ramírez Fuentes <edgar-alejandro-fuentes@hotmail.com>
2020-12-11 02:10:08 -0600

    Inicio del sistema de venta

---
+++ sistema/main.c
@@ -0,0 +1,14 @@
+/* Bibliotecas necesarias para el uso de hilos */
+
+#include "main.h"
+
+/* Función de los thread */
+void *mostrarMenu (void *args) {
+    
+    pthreads_exit(NULL);
+}
+
+
+int main() {
+    pthread_t punto_de_venta1;
+}
